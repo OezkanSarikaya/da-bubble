6ff4d2f5d79094b87d58ab2aa2f09c7dda910fdf
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-09-18 17:06:31 +0200

    Login hard core/done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RecoverPasswordComponent } from './recover-password/recover-password.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
-  {path: "", component: LoginComponent}
+  {path: "", component: LoginComponent},
+  {path: "recover-password", component: RecoverPasswordComponent},
+  {path: "register", component: RegisterComponent}
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  clearPlaceholder(event: any) {
+    event.target.placeholder = '';
+  }
 
+  restorePlaceholder(event: any, placeholderText: string) {
+    if (!event.target.value) {
+      event.target.placeholder = placeholderText;
+    }
+  }
 }
