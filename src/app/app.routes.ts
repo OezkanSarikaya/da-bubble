c312e082792cd554212e52b91a79005ba4111506
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-09-19 16:41:49 +0200

    Imprint/Privacy component created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { LoginComponent } from './login/login.component';
 import { RecoverPasswordComponent } from './recover-password/recover-password.component';
 import { RegisterComponent } from './register/register.component';
 import { MainComponent } from './main/main.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
 export const routes: Routes = [
   {path: "", component: LoginComponent},
   {path: "recover-password", component: RecoverPasswordComponent},
   {path: "register", component: RegisterComponent},
-  {path: "main", component: MainComponent}
+  {path: "main", component: MainComponent},
+  {path: "imprint", component: ImprintComponent},
+  {path: "privacy", component: PrivacyComponent}
 ];
--- src/app/recover-password/recover-password.component.ts
+++ src/app/recover-password/recover-password.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-recover-password',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './recover-password.component.html',
   styleUrl: './recover-password.component.scss'
 })
 export class RecoverPasswordComponent {
+  clearPlaceholder(event: any) {
+    event.target.placeholder = '';
+  }
 
+  restorePlaceholder(event: any, placeholderText: string) {
+    if (!event.target.value) {
+      event.target.placeholder = placeholderText;
+    }
+  }
 }
