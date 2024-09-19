0a5f09b69ff8827a626dfeb84df6b06fa9b0d902
Özkan Sarikaya <oezkan.sarikaya@freenet.de>
2024-09-19 10:00:07 +0200

    login fixed and main components created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RecoverPasswordComponent } from './recover-password/recover-password.component';
 import { RegisterComponent } from './register/register.component';
+import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
   {path: "", component: LoginComponent},
   {path: "recover-password", component: RecoverPasswordComponent},
-  {path: "register", component: RegisterComponent}
+  {path: "register", component: RegisterComponent},
+  {path: "main", component: MainComponent}
 ];
