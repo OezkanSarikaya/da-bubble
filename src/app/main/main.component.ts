0304e6e9ab72c2d515c3b41c1deff4c2a3c1a68a
Özkan Sarikaya <oezkan.sarikaya@freenet.de>
2024-09-19 14:03:38 +0200

    Setup Workspace Menu

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { WorkspaceComponent } from './workspace/workspace.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,WorkspaceComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
