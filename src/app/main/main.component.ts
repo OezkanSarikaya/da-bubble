cf6486fc82f4e204f4537823a3dbbe42624e897a
Özkan Sarikaya <oezkan.sarikaya@freenet.de>
2024-09-19 15:51:01 +0200

    Setup Channel Section and some responsive Work

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { WorkspaceComponent } from './workspace/workspace.component';
+import { ChannelComponent } from "./channel/channel.component";
+import { ThreadComponent } from './thread/thread.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent,WorkspaceComponent],
+  imports: [HeaderComponent, WorkspaceComponent, ChannelComponent,ThreadComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
