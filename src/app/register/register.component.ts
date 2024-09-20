a057b330ff5781932628dded9b29bdda742f7de6
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-09-20 11:07:33 +0200

    register done

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+type register = {
+  name: boolean,
+  email: boolean,
+  password: boolean
+}
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  isAllField: boolean = true;
+  errorForm: register = {
+    name: true,
+    email: true,
+    password: true,
+  }
+  
+  clearPlaceholder(event: any) {
+    event.target.placeholder = '';
+  }
 
+  restorePlaceholder(event: any, placeholderText: string) {
+    if (!event.target.value) {
+      event.target.placeholder = placeholderText;
+    }
+  }
 }
