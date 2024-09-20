2397d94dac333a7e4882ae2cdcec80d3c108f041
Özkan Sarikaya <oezkan.sarikaya@freenet.de>
2024-09-20 08:36:04 +0200

    Create Search Component and add Online Status to user in Workspace Menu

--- src/app/main/workspace/workspace.component.ts
+++ src/app/main/workspace/workspace.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SearchComponent } from '../search/search.component';
 
 @Component({
   selector: 'app-workspace',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,SearchComponent],
   templateUrl: './workspace.component.html',
   styleUrl: './workspace.component.scss',
 })
