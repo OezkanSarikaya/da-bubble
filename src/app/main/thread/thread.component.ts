455602c9c64d32a0df22b85a9f37d37e03f1a08b
Özkan Sarikaya <oezkan.sarikaya@freenet.de>
2024-09-24 12:24:14 +0200

    close function for thread section added

--- src/app/main/thread/thread.component.ts
+++ src/app/main/thread/thread.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { ChatmsgboxComponent } from '../chatmsgbox/chatmsgbox.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-thread',
   standalone: true,
-  imports: [ChatmsgboxComponent],
+  imports: [ChatmsgboxComponent,CommonModule],
   templateUrl: './thread.component.html',
   styleUrl: './thread.component.scss'
 })
 export class ThreadComponent {
+  isThreadOpen = true;
+  toggleThread() {
+    this.isThreadOpen = !this.isThreadOpen;
+  }
 
 }
