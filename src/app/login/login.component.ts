f0de4a71eb3941b1301e154a0a4027639266552c
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-10-13 11:34:36 +0200

    login/message done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,6 +5,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { UserService } from '../services/user.service';
 import { Login } from '../interfaces/login';
 import { FormsModule, NgForm } from '@angular/forms';
+import { MessageComponent } from '../shared/message/message.component';
 
 type loginError = {
   email: boolean,
@@ -14,7 +15,7 @@ type loginError = {
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule, CommonModule, FormsModule],
+  imports: [RouterModule, CommonModule, FormsModule, MessageComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -22,7 +23,7 @@ export class LoginComponent {
   //Chane here false after coding
   isPresentation:boolean = true;
   isAllField: boolean = true;
-  
+  showMessage: boolean = false;
   personLogin: Login ={
     email: '',
     password: ''
@@ -38,12 +39,22 @@ export class LoginComponent {
     this.isPresentation = true;
   }
 
+  animationMessage(){
+    this.showMessage = true;
+    setTimeout(() => {
+      this.showMessage = false;
+    }, 2000);
+  }
+
   async login(ngForm: NgForm){
     if (ngForm.submitted && ngForm.form.valid) {
       const userCredential = await this.userService.login(this.personLogin.email, this.personLogin.password);
       if(userCredential.user && userCredential.user.email){
         console.log(userCredential);
-        this.router.navigate(['/main']);
+        this.animationMessage();
+        setTimeout(() => {
+          this.router.navigate(['/main']);
+        }, 3000);
       }else{
         console.error('Login failed');
       }
