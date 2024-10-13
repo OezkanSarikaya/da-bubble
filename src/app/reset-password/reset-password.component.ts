60cc46d76ea5a1561ab64e302c581907ed58a591
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-10-13 13:30:41 +0200

    reset-password/message showed done

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { UserService } from '../services/user.service';
+import { MessageComponent } from '../shared/message/message.component';
 
 @Component({
   selector: 'app-reset-password',
   standalone: true,
-  imports: [RouterModule, FormsModule, CommonModule],
+  imports: [RouterModule, FormsModule, CommonModule, MessageComponent],
   templateUrl: './reset-password.component.html',
   styleUrl: './reset-password.component.scss'
 })
@@ -17,24 +18,42 @@ export class ResetPasswordComponent {
     password: "",
     confirmPassword: ""
   }
+
   oobCode: string | null = null;
+  showMessage: boolean = false;
+  messageToShow: string = '';
+  messages = {
+    success: 'Anmelden',
+    failed: 'Passwörter stimmen nicht überein'
+  }
 
   constructor(private route: ActivatedRoute, private userService: UserService, private router: Router) {}
 
   ngOnInit(): void {
     this.oobCode = this.route.snapshot.queryParamMap.get('oobCode');
-    console.log(this.oobCode);
   }
 
-  sendPassword(ngForm: NgForm){
+  animationMessage(){
+    this.showMessage = true;
+    setTimeout(() => {
+      this.showMessage = false;
+    }, 2000);
+  }
+
+  sendPassword(ngForm: NgForm){ 
     if (ngForm.submitted && ngForm.form.valid) {
       if(this.person.password === this.person.confirmPassword){
-        this.userService.resetPassword(this.oobCode, this.person.password)
-        console.log(ngForm);
+        this.userService.resetPassword(this.oobCode, this.person.password);
+        this.messageToShow = this.messages.success;
+        this.animationMessage();
+        setTimeout(() => {
+          this.router.navigate(['/']);
+        }, 3000);
       }else{
-        console.log('password are wrong');
+        this.messageToShow = this.messages.failed;
+        this.animationMessage();
       }
-      this.router.navigate(['/']);
+      
     }
   }
 }
