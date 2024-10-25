d65b8f72e782a7cd6591a3e2f69a6f6bff7ebd4e
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-10-25 16:41:35 +0200

    SavingImgae/Edit User dont work yet

--- src/app/shared/user-edit/user-edit.component.ts
+++ src/app/shared/user-edit/user-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { UserService } from '../../services/user.service';
 import { FormsModule, NgForm } from '@angular/forms';
@@ -16,6 +16,7 @@ import { CommonModule } from '@angular/common';
 export class UserEditComponent {
   currentUser!: any
   subscription: Subscription = new Subscription();
+  personAvatar!: string;
   avatars = [
 		"./assets/img/img_profile/profile1.png",
 		"./assets/img/img_profile/profile2.png",
@@ -24,15 +25,19 @@ export class UserEditComponent {
 		"./assets/img/img_profile/profile5.png",
 		"./assets/img/img_profile/profile6.png",
 	];
+  avatarSelected!: string;
   showSelectAvatars: boolean = true;
+  @ViewChild('fileInput') fileInput: any;
 
   constructor(private store:Store<any>, private userService: UserService){}
 
   ngOnInit(): void {
     const sub = this.userService.currentUser$.subscribe(user => {
-      this.currentUser = user;      
+      this.currentUser = user;  
+      this.personAvatar = this.currentUser.avatar;    
     });
     this.subscription.add(sub);
+    this.avatarSelected = this.currentUser.avatar;
   }
 
   ngOnDestroy(): void {
@@ -42,6 +47,54 @@ export class UserEditComponent {
   triggerShowHideSelectAvatars(){
     this.showSelectAvatars = !this.showSelectAvatars;
   }
+
+  selectImage($event: any) {
+		const file = $event.target.files[0];
+    if (file) {
+			//Keep real file to be oploaded
+			// Mantener el archivo real para la subida
+			this.avatarSelected = file;
+
+			//Load the image in base64 to be showed
+			// Cargar la imagen en base64 para mostrar una vista previa (opcional)
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.personAvatar  = e.target.result; //Only to show like preview view // Esto solo es para vista previa
+      };
+      reader.readAsDataURL(file);  //Read image in base64 format // Leer la imagen como base64 para previsualización
+    }
+  }
+
+  selectAvatar(avatarURL: string){
+    this.personAvatar = avatarURL;
+  }
+
+  openFile(){
+		this.fileInput.nativeElement.click();
+	}
+
+  async savingImgAvatar(){
+     // Verificar si `avatarSelected` es una URL de una de las imágenes en `avatars`
+      const isPredefinedAvatar = this.avatars.includes(this.avatarSelected);
+      if(isPredefinedAvatar){
+        let idRef = await this.userService.findUserByField('uid',this.currentUser.uid);
+        if(idRef){
+          console.log(idRef.ref);
+        }
+        
+      }else{
+        console.log('is not');
+      }
+		// let avatarUrl: string = this.personAvatar;
+    
+		// if(this.avatarSelected){
+		// 	const uploadResult   = await this.userService.uploadImage(this.avatarSelected, this.currentUser.fullName);
+		// 	if(uploadResult  ){
+		// 		avatarUrl = uploadResult
+		// 	}
+		// }
+		// this.personAvatar = avatarUrl ;
+	}
 
   async onSubmitEdit(editForm: NgForm){
     if (editForm.submitted && editForm.form.valid) {
@@ -57,7 +110,4 @@ export class UserEditComponent {
     this.store.dispatch(hideUserProfile());
   }
 
-  selectAvatar(profile: any){
-    console.log(profile);
-  }
 }
