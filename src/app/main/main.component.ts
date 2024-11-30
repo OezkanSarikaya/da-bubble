ba73edb5fc001bd3ca2798519e711c6dffaf7ce0
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-11-30 13:10:45 +0100

    Profile component fixed

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,9 +7,12 @@ import { CommonModule } from '@angular/common';
 import { PopupUserProfileComponent } from '../shared/popup-user-profile/popup-user-profile.component';
 import { Store } from '@ngrx/store';
 import { hideThreadComponent, hideUserProfile, showNewMessage, triggerPopUserProfile } from '../state/actions/triggerComponents.actions';
-import { Observable, take } from 'rxjs';
+import { filter, from, map, Observable, of, startWith, Subscription, switchMap, take, tap } from 'rxjs';
 import { showHideThreadSelector, showHideUserEditProfileHeaderSelector, triggerChannelSelector, triggerNewMessageSelector, triggerUserProfilePopUpSelector } from '../state/selectors/triggerComponents.selectors';
 import { UserEditComponent } from '../shared/user-edit/user-edit.component';
+import { UserService } from '../services/user.service';
+import { User } from '../interfaces/user';
+import { ChannelService } from '../services/channel.service';
 
 @Component({
   selector: 'app-main',
@@ -34,8 +37,10 @@ export class MainComponent {
   isChannelSelected$: Observable<boolean> = new Observable();
   userEditProfile$: Observable<boolean> = new Observable(); 
   isNewMessageOpen$: Observable<boolean> = new Observable();
+  subscription: Subscription = new Subscription();
+  currentUser!: User;
 
-  constructor(private store:Store<any>){}
+  constructor(private store:Store<any>, private userService: UserService, private channelService: ChannelService){}
 
   ngOnInit(): void {
     this.userProfilePopUp$ = this.store.select(triggerUserProfilePopUpSelector);
@@ -43,6 +48,27 @@ export class MainComponent {
     this.userEditProfile$ = this.store.select(showHideUserEditProfileHeaderSelector);
     this.isNewMessageOpen$ = this.store.select(triggerNewMessageSelector);
     this.isChannelSelected$ = this.store.select(triggerChannelSelector);
+    const subUser = this.userService.currentUser$.pipe(
+      startWith(null),
+      filter(user => !user || (user && user.idFirebase)),
+      switchMap(
+        (user: any) => {
+          if (!user) {
+            return of(null);
+          }
+          return  this.channelService.fetchUserAsObservable(user.idFirebase)
+        }
+      )
+    ).subscribe(userFound => {
+      if(userFound){
+        this.currentUser = userFound;
+      }
+    })
+    this.subscription.add(subUser);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();    
   }
 
   hideChannel() {
--- src/app/shared/popup-user-profile/popup-user-profile.component.ts
+++ src/app/shared/popup-user-profile/popup-user-profile.component.ts
@@ -1,28 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { hideUserProfile, triggerPopUserProfile } from '../../state/actions/triggerComponents.actions';
 import { UserService } from '../../services/user.service';
 import { Subscription } from 'rxjs';
+import { User } from '../../interfaces/user';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-popup-user-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './popup-user-profile.component.html',
   styleUrl: './popup-user-profile.component.scss'
 })
 export class PopupUserProfileComponent {
   avatar: string = './assets/img/img_profile/profile1.png';
-  currentUser!: any
   subscription: Subscription = new Subscription();
+  areYou!: any
+  @Input() currentUser!: User
 
   constructor(private store:Store<any>, private userService: UserService){}
 
   ngOnInit(): void {
-    const sub = this.userService.currentUser$.subscribe(user => {
-      this.currentUser = user;      
+    const subAreYou = this.userService.currentUser$.subscribe(user => {
+      this.areYou = user;    
+      console.log(this.areYou); 
     });
-    this.subscription.add(sub);
+    this.subscription.add(subAreYou);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['currentUser'] && changes['currentUser'].currentValue) {
+      changes['currentUser'].currentValue;
+      console.log(this.currentUser);
+    }
   }
 
   ngOnDestroy(): void {
